a1ba1352ec1d35fa8f73d094cc3c9f5b2973598b
Sariel Har-Peled <sariel@gmail.com>
2026-09-13 19:16:17 -0500

    Bump version to 1.0.21

--- lib/latex_it/version.rb
+++ lib/latex_it/version.rb
@@ -7,7 +7,7 @@
 # ==============================================================================
 
 module LatexIt
-  VERSION = '1.0.20'.freeze
+  VERSION = '1.0.21'.freeze
   EXECUTABLE = File.expand_path('../../latex_it', __dir__).freeze
 end
 

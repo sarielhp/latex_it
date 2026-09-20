b1c31b019d93874e6287d285e9398e23881f56af
Sariel Har-Peled <sariel@gmail.com>
2026-09-20 17:06:50 -0500

    feat(bump): abort immediately when running inside a Bubblewrap environment

---
+++ test/test_bump.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'open3'
+
+class TestBump < Minitest::Test
+  BUMP_BIN = File.expand_path('../tools/bump', __dir__)
+
+  def test_bump_rejects_bubblewrap_environment
+    out, status = Open3.capture2e(BUMP_BIN)
+    refute status.success?
+    assert_equal 1, status.exitstatus
+    assert_includes out, 'Cannot bump version: running inside a Bubblewrap environment'
+    assert_includes out, 'Please run ./tools/bump directly on the host machine'
+  end
+
+  def test_bump_dry_run_also_rejects_bubblewrap_environment
+    out, status = Open3.capture2e(BUMP_BIN, '--dry-run')
+    refute status.success?
+    assert_equal 1, status.exitstatus
+    assert_includes out, 'Cannot bump version: running inside a Bubblewrap environment'
+  end
+
+  def test_bump_help_still_works_inside_bubblewrap
+    out, status = Open3.capture2e(BUMP_BIN, '--help')
+    assert status.success?
+    assert_includes out, 'Usage: ./tools/bump [options]'
+    assert_includes out, '--dry-run'
+  end
+end
